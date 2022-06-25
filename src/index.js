896ba5aff002d05ecc8e8a0dcbfdc9f494eb6b2d
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2022-06-25 18:06:33 -0300

    feat: checksTodoExists middleware

--- src/index.js
+++ src/index.js
@@ -33,7 +33,20 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).json({ error: "User not found" });
+
+  const { id } = request.params;
+  const isValidUUID = validate(id);
+  if (!isValidUUID) return response.status(400).json();
+
+  const todo = user.todos.find((todo) => todo.id == id);
+  if (!todo) return response.status(404).json({ error: "Todo not found" });
+
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
